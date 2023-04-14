355c134136e101763f3e554a293518a55504cee8
Yash Khemani <yash@khemani.com>
2023-04-14 12:29:35 +0000

    add snowflake_role_grants

--- main.tf
+++ main.tf
@@ -4,9 +4,15 @@ resource "random_password" "password" {
 }
 
 resource "snowflake_user" "user" {
-  name         = var.snowflake_username
-  password     = random_password.password.result
-  default_role = var.role_name
+  name              = var.snowflake_username
+  password          = random_password.password.result
+  default_role      = var.role_name
+  default_warehouse = "COMPUTE_WH"
+}
+
+resource "snowflake_role_grants" "grants" {
+  role_name = var.role_name
+  users     = [snowflake_user.user.name]
 }
 
 resource "vault_mount" "db" {
